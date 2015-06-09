6335cbaa3c3a4a6fb73a79b50a19f92182bea78b
Daniel Bachhuber <d@danielbachhuber.com>
2015-06-09 12:35:19 -0700

    Initial commit

---
+++ publishing-checklist.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Publishing Checklist
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: Fusion Engineering
+Author URI: http://fusion.net/
+Plugin URI: PLUGIN SITE HERE
+Text Domain: publishing-checklist
+Domain Path: /languages
+*/
+
