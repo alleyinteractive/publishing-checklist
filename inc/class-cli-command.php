a6b02580d642529de8dad1a9ac71b5da05fcc9f9
davisshaver <davisshaver@users.noreply.github.com>
2015-06-19 09:47:10 -0400

    Fixes for PHPCS

--- inc/class-cli-command.php
+++ inc/class-cli-command.php
@@ -5,26 +5,26 @@
  */
 class Publishing_Checklist extends WP_CLI_Command {
 
-    /**
-     * Returns editorial checklist for a given post.
-     * 
-     * ## OPTIONS
-     * 
-     * <id>
-     * : The id of the post 
-     * 
-     * ## EXAMPLES
-     * 
-     *     wp checklist evaluate 1
-     *
-     * @synopsis <id>
-     */
-    function evaluate( $args = array(), $assoc_args = array() ) {
-        list( $id ) = $args;
+	/**
+	 * Returns editorial checklist for a given post.
+	 * 
+	 * ## OPTIONS
+	 * 
+	 * <id>
+	 * : The id of the post 
+	 * 
+	 * ## EXAMPLES
+	 * 
+	 *     wp checklist evaluate 1
+	 *
+	 * @synopsis <id>
+	 */
+	function evaluate( $args = array(), $assoc_args = array() ) {
+		list( $id ) = $args;
 
-        // Print a success message
-        WP_CLI::success( "Evaluated $id" );
-    }
+		// Print a success message
+		WP_CLI::success( "Evaluated $id" );
+	}
 }
 
 WP_CLI::add_command( 'checklist', 'Publishing_Checklist' );
