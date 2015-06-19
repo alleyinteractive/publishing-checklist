ded5cc18c0052f743fd3acbdaa2a6339562d5c6d
davisshaver <davisshaver@users.noreply.github.com>
2015-06-19 09:37:51 -0400

    Porto structure for WP CLI command

---
+++ inc/class-cli-command.php
@@ -0,0 +1,32 @@
+<?php
+
+use WP_CLI;
+
+/**
+ * Helper methods for both migration components
+ */
+class Publishing_Checklist extends WPCOM_VIP_CLI_Command {
+
+    /**
+     * Returns editorial checklist for a given post.
+     * 
+     * ## OPTIONS
+     * 
+     * <id>
+     * : The id of the post 
+     * 
+     * ## EXAMPLES
+     * 
+     *     wp checklist evaluate 1
+     *
+     * @synopsis <id>
+     */
+    function evaluate( $args, $assoc_args ) {
+        list( $id ) = $args;
+
+        // Print a success message
+        WP_CLI::success( "Evaluated $id" );
+    }
+}
+
+WP_CLI::add_command( 'checklist', 'Publishing_Checklist' );
