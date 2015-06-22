d2e5b49c4efa3935b97fff5993a2ec18cccc3c71
davisshaver <davis@daviss-MacBook.local>
2015-06-22 13:02:15 -0400

    Fix indenting

--- inc/class-cli-command.php
+++ inc/class-cli-command.php
@@ -21,17 +21,17 @@ class CLI_Command extends WP_CLI_Command {
 	function evaluate( $args = array(), $assoc_args = array() ) {
 		list( $id ) = $args;
 		$checklist_data = Publishing_Checklist()->checklist_evaluate( $id );
-		if ( $checklist_data === false ) {
+		if ( empty( $checklist_data ) ) {
 			WP_CLI::error( 'No checklist found.' );
 		}
 		WP_CLI::success( sprintf( __( '%d of %d tasks complete', 'publishing-checklist' ), count( $checklist_data['tasks'] ), count( $checklist_data['completed'] ) ) );
 		foreach ( $checklist_data['tasks'] as $id => $task ) :
 			if ( in_array( $id, $checklist_data['completed'] ) ) :
 				WP_CLI::line( '+ ' .  $task['explanation'] );
-			 else :
+			else :
 				WP_CLI::line( '- ' .  $task['explanation'] );
 			endif;
-		endforeach; 
+		endforeach;
 	}
 }
 
