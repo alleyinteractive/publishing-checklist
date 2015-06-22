39bb7efccdc79aa6542766b8077132abad3af1d9
davisshaver <davis@daviss-MacBook.local>
2015-06-22 17:23:47 -0400

    Update command to work with one or more ID's

--- inc/class-cli-command.php
+++ inc/class-cli-command.php
@@ -1,7 +1,8 @@
 <?php
+use \WP_CLI\Utils;
 
 /**
- * Helper methods for both migration components
+ * Based on: 
  */
 class CLI_Command extends WP_CLI_Command {
 
@@ -10,29 +11,48 @@ class CLI_Command extends WP_CLI_Command {
 	 *
 	 * ## OPTIONS
 	 *
-	 * <id>
-	 * : The id of the post
+	 * <id>...
+	 * : The ID of one or more posts
 	 *
+	 * [--format=<format>]
+	 * : Accepted values: table, json, csv. Default: table
 	 * ## EXAMPLES
 	 *
 	 *     wp checklist evaluate 1
 	 *
-	 * @synopsis <id>
 	 */
-	function evaluate( $args = array(), $assoc_args = array() ) {
-		list( $id ) = $args;
-		$checklist_data = Publishing_Checklist()->evaluate_checklist( $id );
-		if ( empty( $checklist_data ) ) {
-			WP_CLI::error( 'No checklist found.' );
+	public function evaluate( $args = array(), $assoc_args = array() ) {
+        $fields = array(
+        	'status',
+        	'label',
+        	'explanation',
+        );
+		foreach( $args as $id ) {
+			$checklist_data = Publishing_Checklist()->evaluate_checklist( $id );
+
+			if ( empty( $checklist_data ) ) {
+				WP_CLI::error( sprintf( __( 'No checklist found for %d.', 'publishing-checklist' ), $id ) );;
+			}
+
+			WP_CLI::success( sprintf( __( '%d of %d tasks complete for %d', 'publishing-checklist' ), count( $checklist_data['completed'] ), count( $checklist_data['tasks'] ), $id ) );
+
+			$key = $id;
+			$cli_evaluation = array();
+			foreach ( $checklist_data['tasks'] as $id => $task ) :
+				if ( in_array( $id, $checklist_data['completed'] ) ) :
+					$cli_evaluation[ $key ][ 'status' ] = '+';
+					$cli_evaluation[ $key ][ 'label' ] =  $task['label'];
+					$cli_evaluation[ $key ][ 'explanation' ] =  $task['explanation'];
+				else :
+					$cli_evaluation[ $key ][ 'status' ] = '-';
+					$cli_evaluation[ $key ][ 'label' ] =  $task['label'];
+					$cli_evaluation[ $key ][ 'explanation' ] =  $task['explanation'];
+				endif;
+				$key++;
+			endforeach;
+    		\WP_CLI\Utils\format_items( $assoc_args['format'], $cli_evaluation, $fields );
 		}
-		WP_CLI::success( sprintf( __( '%d of %d tasks complete', 'publishing-checklist' ), count( $checklist_data['tasks'] ), count( $checklist_data['completed'] ) ) );
-		foreach ( $checklist_data['tasks'] as $id => $task ) :
-			if ( in_array( $id, $checklist_data['completed'] ) ) :
-				WP_CLI::line( '+ ' .  $task['explanation'] );
-			else :
-				WP_CLI::line( '- ' .  $task['explanation'] );
-			endif;
-		endforeach;
+
 	}
 }
 
