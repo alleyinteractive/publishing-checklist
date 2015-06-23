6c822bc2f57c83cec9c51ba0dec815104ddeb8bc
davisshaver <davis@daviss-MacBook.local>
2015-06-23 09:59:37 -0400

    Consolidate table output – new row for each task/post

--- inc/class-cli-command.php
+++ inc/class-cli-command.php
@@ -28,12 +28,14 @@ class Evaluate_Checklist_CLI_Command extends WP_CLI_Command {
 		$values = wp_parse_args( $assoc_args, $defaults );
 
 		$fields = array(
+			'task_id',
 			'id',
 			'status',
 			'label',
 			'explanation',
 		);
 		$cli_evaluation = array();
+		$key = 0;
 		foreach ( $args as $post_id ) {
 			$checklist_data = Publishing_Checklist()->evaluate_checklist( $post_id );
 
@@ -43,25 +45,26 @@ class Evaluate_Checklist_CLI_Command extends WP_CLI_Command {
 			}
 
 			WP_CLI::success( sprintf( __( '%d of %d tasks complete for %d', 'publishing-checklist' ), count( $checklist_data['completed'] ), count( $checklist_data['tasks'] ), $post_id ) );
-			$key = 0;
+			
 			foreach ( $checklist_data['tasks'] as $id => $task ) {
 				if ( in_array( $id, $checklist_data['completed'] ) ) :
 					$cli_evaluation[ $key ]['id'] = $post_id;
+					$cli_evaluation[ $key ]['task_id'] = $id;
 					$cli_evaluation[ $key ]['status'] = '+';
 					$cli_evaluation[ $key ]['label'] = $task['label'];
 					$cli_evaluation[ $key ]['explanation'] = $task['explanation'];
 				else :
 					$cli_evaluation[ $key ]['id'] = $post_id;
+					$cli_evaluation[ $key ]['task_id'] = $id;
 					$cli_evaluation[ $key ]['status'] = '-';
 					$cli_evaluation[ $key ]['label'] = $task['label'];
 					$cli_evaluation[ $key ]['explanation'] = $task['explanation'];
 				endif;
 				$key++;
 			}
-			\WP_CLI\Utils\format_items( $values['format'], $cli_evaluation, $fields );
-
+			
 		}
-
+		\WP_CLI\Utils\format_items( $values['format'], $cli_evaluation, $fields );
 	}
 }
 
