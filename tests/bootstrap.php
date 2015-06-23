329dc8f86929c39fba729b66f97cfdfef4e4e927
davisshaver <davis@daviss-MacBook.local>
2015-06-23 09:16:24 -0400

    Move test function to inc folder

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,19 +9,8 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/publishing-checklist.php';
+	require dirname( dirname( __FILE__ ) ) . '/inc/functions.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
-
-function ensure_minimum_200_words( $post_id, $id ) {
-
-	$obj = get_post( $post_id );
-
-	if ( ! $obj ) {
-		return false;
-	}
-	$word_count = str_word_count( strip_tags( $obj->post_content ) );
-
-	return ( $word_count > 200 );
-}
