0461984145b96594a9a3a211e55c9773a06be8e5
davisshaver <davis@x-160-94-31-213.uofm-secure.wireless.umn.edu>
2015-06-26 08:59:07 -0500

    Model publishing checklist JS binding after core

--- assets/js/src/publishing-checklist.js
+++ assets/js/src/publishing-checklist.js
@@ -1,12 +1,12 @@
 (function($){
 
-	$('.publishing-checklist-show-list').on('click', function() {
+	$('#the-list').on('click', '.publishing-checklist-show-list', function(){
 		$( '.publishing-checklist-items', $(this).parent() ).show();
 		$(this).hide();
 		$( '.publishing-checklist-hide-list', $(this).parent() ).show();
 	});
 
-	$('.publishing-checklist-hide-list').on('click', function() {
+	$('#the-list').on('click', '.publishing-checklist-hide-list', function(){
 		$( '.publishing-checklist-items', $(this).parent() ).hide();
 		$(this).hide();
 		$( '.publishing-checklist-show-list', $(this).parent() ).show();
