b671004f7087930f90906b32511474c340133ea4
davisshaver <davis@x-160-94-31-213.uofm-secure.wireless.umn.edu>
2015-06-26 09:38:59 -0500

    Refactor jS to handle both Manage & Edit

--- assets/js/src/publishing-checklist.js
+++ assets/js/src/publishing-checklist.js
@@ -1,15 +1,23 @@
 (function($){
 
-	$('#the-list').on('click', '.publishing-checklist-show-list', function(){
+	$('#the-list').on('click', '.publishing-checklist-show-list', showChecklist );
+
+	$('#the-list').on('click', '.publishing-checklist-hide-list', hideChecklist );
+
+	$('.publishing-checklist').on('click', '.publishing-checklist-show-list', showChecklist );
+
+	$('.publishing-checklist').on('click', '.publishing-checklist-hide-list', hideChecklist );
+
+	function showChecklist(event) {
 		$( '.publishing-checklist-items', $(this).parent() ).show();
 		$(this).hide();
 		$( '.publishing-checklist-hide-list', $(this).parent() ).show();
-	});
-
-	$('#the-list').on('click', '.publishing-checklist-hide-list', function(){
+	}
+	function hideChecklist(event) {
 		$( '.publishing-checklist-items', $(this).parent() ).hide();
 		$(this).hide();
 		$( '.publishing-checklist-show-list', $(this).parent() ).show();
-	});
+	}
 
 }(jQuery));
+
