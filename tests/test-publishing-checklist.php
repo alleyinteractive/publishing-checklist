30c02d2e7e4b88caeb33f9f3d0d2d7ffa91ccb15
Matth_eu <matthew@matth.eu>
2016-02-10 11:08:53 +0000

    Add unit test for multiple post types

--- tests/test-publishing-checklist.php
+++ tests/test-publishing-checklist.php
@@ -10,7 +10,7 @@ Maecenas nunc nisi, pulvinar dapibus sagittis non, accumsan id turpis.
 
 Nulla facilisi. Cras fringilla eu neque vitae sagittis. Aliquam vulputate, metus ut euismod ultricies, nunc neque convallis justo, eu blandit urna felis a nulla. Maecenas consectetur odio magna, eget eleifend ex accumsan non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras eget neque neque. Morbi ut vehicula eros, eu feugiat orci. Suspendisse sodales nec nisl nec finibus. Nunc porta euismod justo et pellentesque. Mauris convallis elit eget ligula elementum aliquam. Duis consequat lacus nec nulla maximus, ac pharetra dui interdum. Mauris et est ut enim auctor euismod non in urna. Sed ut sollicitudin elit. Pellentesque maximus nisl vel nulla tempus, feugiat venenatis lorem convallis.
 
-Fusce tincidunt finibus mi vel porta. 
+Fusce tincidunt finibus mi vel porta.
 
 Fusce tincidunt finibus mi vel porta. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent at leo eu quam ullamcorper fermentum vel nec lorem. Praesent maximus aliquam felis. Suspendisse ac nisl velit. Aliquam non mattis magna, quis elementum elit. Etiam nulla augue, suscipit malesuada sollicitudin vitae, malesuada vitae risus. Sed diam eros, bibendum in condimentum ac, porta ullamcorper lorem. Praesent in dignissim ante, non auctor est. Quisque placerat.';
 
@@ -26,6 +26,7 @@ Fusce tincidunt finibus mi vel porta. Cum sociis natoque penatibus et magnis dis
 		parent::setUp();
 
 		self::$instance = new Publishing_Checklist;
+
 		$args = array(
 			'label'           => esc_html__( 'Word Count', 'publishing-checklist' ),
 			'callback'        => 'ensure_minimum_200_words',
@@ -33,6 +34,7 @@ Fusce tincidunt finibus mi vel porta. Cum sociis natoque penatibus et magnis dis
 			'post_type'       => array( 'post' ),
 		);
 		Publishing_Checklist()->register_task( 'test-publishing-checklist-word-count', $args );
+
 	}
 
 	public function test_incomplete_evaluate_checklist() {
@@ -53,6 +55,22 @@ Fusce tincidunt finibus mi vel porta. Cum sociis natoque penatibus et magnis dis
 		$this->assertContains( 'Word Count', $evaluated['tasks']['test-publishing-checklist-word-count']['label'] );
 		$this->assertContains( 'Posts should be at least 200 words.', $evaluated['tasks']['test-publishing-checklist-word-count']['explanation'] );
 		$this->assertContains( 'test-publishing-checklist-word-count', $evaluated['completed'] );
+	}
+
+	public function test_tasks_for_multiple_post_types() {
+
+		Publishing_Checklist()->register_task( 'test-pass-page', array(
+			'label'           => 'test',
+			'callback'        => '__return_true',
+			'post_type'       => array( 'page' ),
+		) );
+
+		$post_id = $this->factory->post->create( array( 'post_type' => 'page' ) );
+		$evaluated = Publishing_Checklist()->evaluate_checklist( $post_id );
+
+		$this->assertEquals( 1, count( $evaluated['tasks'] ) );
+		$this->assertTrue( in_array( 'test-pass-page', $evaluated['completed'] ) );
+
 	}
 
 	public function test_checklist_column_output_action() {
